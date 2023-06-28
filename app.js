6ed09b7397d619e2d2d1a9870e1a7e35729a3e3f
Emily <emsager7@gmail.com>
2023-06-28 16:00:56 -0700

    1600 post-Emran conference

--- app.js
+++ app.js
@@ -14,30 +14,37 @@ const noTVImage = "https://images.pexels.com/photos/17233395/pexels-photo-172333
  */
 //console.log("test!1");
 async function searchShows(query) {
+  //console.log(`https://api.tvmaze.com/search/shows?q=${query}`);
   let res = await axios.get(`https://api.tvmaze.com/search/shows?q=${query}`);
   //console.log(res.data);
   
   let shows = res.data.map(result => {
       let show = result.show;
+      console.log(show);
       return {
         id: show.id,
         name: show.name,
         summary: show.summary,
         image: show.image ? show.image.medium : noTVImage,
       };
+      
     });
-    console.log("test2");
-    console.log(shows);
+    //console.log("test2");
+    
   return shows;
 }
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows() {
-  let showsList = $("#shows-list");
-  showsList.empty();
-
-  for (let show of showsList) {
+function populateShows(shows) {
+  // $showsList.empty();
+  console.log(shows);
+  console.log($showsList);
+  for (let show of shows) {
+    console.log(show.id);
+    console.log(show.image);
+    console.log(show.name);
+    console.log(show.summary);
     let showItem = $(
         `<div data-show-id="${show.id}" class="col-md-12 col-lg-6 mb-4" id="Show">
          <div class="card" data-show-id="${show.id}">
@@ -45,6 +52,7 @@ function populateShows() {
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
+
              <button class="btn btn-outline-light btn-sm" id="Show-getEpisodes">
                Episodes
              </button>
@@ -52,9 +60,9 @@ function populateShows() {
          </div>  
        </div>
       `);
-    showsList.append(showItem); 
+    $showsList.append(showItem); 
     
-    console.log(showsList);
+    console.log($showsList);
     console.log("test3");
   };
 }
@@ -64,28 +72,31 @@ function populateShows() {
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
 
-async function searchForShowAndDisplay() {
-  let querys = $("#searchForm").val();
-  let show = await searchShows(querys);
+async function searchForShowAndDisplay(query) {
+  // let querys = $("#searchForm").val();
+  let shows = await searchShows(query);
 
   
  //populateShows(show);
- console.log(show);
-  console.log("test4");
+//  console.log(shows);
+  // console.log("test4");
+  return shows;
 }
 
 $searchForm.on("submit", async function handleSearch(e) {
   e.preventDefault();
 
   let query = $('#search-query').val();
+  console.log(query);
   if(!query) return;
 
   $episodesArea.hide();
 
-  let show = await searchForShowAndDisplay(query);
+  // let shows = await searchForShowAndDisplay(query);
+  let shows = await searchShows(query);
   //console.log(show);
   console.log('A');
-  populateShows(show);
+  populateShows(shows);
   console.log("test5");
 });
 
